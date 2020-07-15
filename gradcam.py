2b8a8bc75ff84f50ba856d80dee88946e47d94d2
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-15 12:05:28 +0530

    Refactored Image Overlay Function

--- gradcam.py
+++ gradcam.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from matplotlib import cm
 
 
 class GradCam:
@@ -50,18 +49,3 @@ class GradCam:
         heatmap = np.mean(last_conv_layer_output, axis=-1)
         heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
         return heatmap
-
-    @staticmethod
-    def get_overlayed_image(image_path, heatmap):
-        img = tf.keras.preprocessing.image.load_img(image_path)
-        img = tf.keras.preprocessing.image.img_to_array(img)
-        heatmap = np.uint8(255 * heatmap)
-        jet = cm.get_cmap("jet")
-        jet_colors = jet(np.arange(256))[:, :3]
-        jet_heatmap = jet_colors[heatmap]
-        jet_heatmap = tf.keras.preprocessing.image.array_to_img(jet_heatmap)
-        jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
-        jet_heatmap = tf.keras.preprocessing.image.img_to_array(jet_heatmap)
-        overlayed_img = jet_heatmap * 0.7 + img
-        overlayed_img = tf.keras.preprocessing.image.array_to_img(overlayed_img)
-        return overlayed_img
--- utils.py
+++ utils.py
@@ -1,44 +1,18 @@
-import cv2
 import numpy as np
-from PIL import Image
 import tensorflow as tf
-from tensorflow import keras
-from matplotlib import pyplot as plt
+from matplotlib import cm
 
 
-def read_image(file_path, image_size):
-    original_image = np.array(Image.open(file_path))
-    image = keras.preprocessing.image.load_img(
-        file_path, target_size=(image_size, image_size)
-    )
-    image = np.array([keras.preprocessing.image.img_to_array(image)])
-    return image, original_image
-
-
-def visualize(image, cam, cam_overlayed):
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(16, 16))
-    plt.setp(axes.flat, xticks=[], yticks=[])
-    for i, ax in enumerate(axes.flat):
-        if i % 3 == 0:
-            ax.imshow(image)
-            ax.set_xlabel('Image')
-        elif i % 3 == 1:
-            ax.imshow(cam)
-            ax.set_xlabel('Cam')
-        else:
-            ax.imshow(cam_overlayed)
-            ax.set_xlabel('Cam Overlayed')
-    plt.show()
-
-
-def get_prediction(image_path: str, model_dict: dict, model_option: str) -> list:
-    image = tf.keras.preprocessing.image.load_img(image_path)
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image = cv2.resize(image, (224, 224))
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    model, preprocessing_function, decoding_function = model_dict[model_option]
-    image = preprocessing_function(image)
-    model = model(weights='imagenet')
-    prediction = model.predict(image)
-    prediction = decoding_function(prediction)
-    return prediction
+def get_overlayed_image(image_path, heatmap, weightage):
+    img = tf.keras.preprocessing.image.load_img(image_path)
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    heatmap = np.uint8(255 * heatmap)
+    jet = cm.get_cmap("jet")
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+    jet_heatmap = tf.keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = tf.keras.preprocessing.image.img_to_array(jet_heatmap)
+    overlayed_img = jet_heatmap * weightage + img
+    overlayed_img = tf.keras.preprocessing.image.array_to_img(overlayed_img)
+    return overlayed_img
